dff199cc448c20ad91aac90ecc427e25f3ae91ea
5inister <diego@trujillodiego.com>
2014-06-12 18:09:07 +0100

    created and tested the remove from buffer script

---
+++ remove_from_buffer.php
@@ -0,0 +1,31 @@
+<?php
+	//open the json, find the iId that has just printed and remove it from the buffer.
+	$dir = $_POST["uId"];
+	$iId = $_POST["iId"];
+	//Get existing buffer file
+	$bufferFile = $dir .'/'. 'buffer.json';
+	//Define an empty output array to push instances to be kept
+	$out_buffer = array();
+	//Convert to array by decoding the json
+	$existing_buffer = json_decode(file_get_contents($bufferFile), true);
+	echo sizeOf($existing_buffer);
+	for ($i=0; $i <sizeof($existing_buffer) ; $i++) { 
+		if ($existing_buffer[$i]['iId']!=$iId){
+			array_push($out_buffer, $existing_buffer[$i]);
+		}
+	}
+	echo sizeOf($out_buffer);
+	$fs1 = fopen($bufferFile,"w");
+	//rencode the json and write back
+	fwrite($fs1, json_encode($out_buffer));
+	fclose($fs1);
+	//echo 1 if there are any elements left in buffer
+	if (0!=sizeOf($out_buffer)){
+		echo 1;
+	}
+	//else echo 0
+	else{
+		echo 0;
+	}
+	
+?>

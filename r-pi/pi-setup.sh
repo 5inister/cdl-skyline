149ada75af7a9624e6d5d1c0a12469925158af25
5inister <diego@trujillodiego.com>
2014-07-08 13:10:55 +0100

    added auto-login to configuration script

--- r-pi/pi-setup.sh
+++ r-pi/pi-setup.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python-pip python-dev python2.7-dev
 sudo pip uninstall pil
+echo "Installing PIL"
 wget http://www.ijg.org/files/jpegsrc.v9.tar.gz
 tar xvfz jpegsrc.v9.tar.gz
 cd jpeg-9c
@@ -14,5 +15,7 @@ sudo ln -s /usr/lib/arm-linux-gnueabi/libjpeg.so /usr/lib <-Check this
 sudo ln -s /usr/lib/arm-linux-gnueabi/libfreetype.so /usr/lib
 sudo ln -s /usr/lib/arm-linux-gnueabi/libz.so /usr/lib
 sudo apt-get install libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
-sudo pip install pil
+sudo pip install
+echo "Adding auto login"
+sudo cp ~/cdl-skyline/r-pi/inittab /etc/
 echo "system ready!"

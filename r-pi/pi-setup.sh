f634809526351acbb6011ffd208f5110daeae493
5inister <diego@trujillodiego.com>
2014-07-08 12:13:34 +0100

    created a quick r-pi setup script that installs dependencies and configures the system

---
+++ r-pi/pi-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install python-pip python-dev python2.7-dev
+sudo pip uninstall pil
+wget http://www.ijg.org/files/jpegsrc.v9.tar.gz
+tar xvfz jpegsrc.v9.tar.gz
+cd jpeg-9c
+./configure --enable-shared --prefix=$CONFIGURE_PREFIX
+make
+sudo make install
+cd ~
+sudo ln -s /usr/lib/arm-linux-gnueabi/libjpeg.so /usr/lib <-Check this
+sudo ln -s /usr/lib/arm-linux-gnueabi/libfreetype.so /usr/lib
+sudo ln -s /usr/lib/arm-linux-gnueabi/libz.so /usr/lib
+sudo apt-get install libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
+sudo pip install pil
+echo "system ready!"
